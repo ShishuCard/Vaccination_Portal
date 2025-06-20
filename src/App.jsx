c6cfd2c6b7dbd8e7bb134f9e420137e439e7f21e
Shreya Ghorui <shreyaghorui202@gmail.com>
2025-06-20 22:28:08 +0530

    Navbar made

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
+import Navbar from "./components/navbar";
 import './App.css'
 
 function App() {
   return (
     <>
-      <h1 class="text-5xl font-bold underline">
-        Hello world!
-      </h1>
+      <Navbar />
     </>
   )
 }
