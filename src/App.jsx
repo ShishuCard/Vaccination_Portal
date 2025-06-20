c0729c64a6c8ee4fdabe06fec206ec228e180718
AnkitShaw_100 <ankitshaw6933@gmail.com>
2025-06-21 03:22:43 +0530

    Doctordash board enhanced

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,34 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Components
 import Navbar from "./components/Navbar";
+import Footer from "./components/footer";
+
+// Pages
 import Home from "./components/Home";
 import DoctorDashboard from "./components/DoctorDashboard";
-// import AboutUs from "./components/AboutUs";
-// import ContactUs from "./components/ContactUs";
+import AboutUs from "./components/AboutUs";
+import ContactUs from "./components/ContactUs";
+// import Login from "./components/login";
+// import SignUp from "./components/signup";
 
 const App = () => {
   return (
     <Router>
-      <div className="min-h-screen bg-[#f8f9fa]">
+      <div className="flex flex-col min-h-screen bg-[#f8f9fa]">
         <Navbar />
-        <main className="px-4 py-6">
+        <main className="flex-grow px-4 py-6">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/doctor-dashboard" element={<DoctorDashboard />} />
-            {/* <Route path="/about" element={<AboutUs />} />
-            <Route path="/contact" element={<ContactUs />} /> */}
+            <Route path="/about" element={<AboutUs />} />
+            <Route path="/contact" element={<ContactUs />} />
+            {/* <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} /> */}
           </Routes>
         </main>
+        <Footer />
       </div>
     </Router>
   );
