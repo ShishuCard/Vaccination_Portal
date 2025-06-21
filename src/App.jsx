dd8a85d9c8a93be48ca8251300189fca36d2a6e3
AnkitShaw_100 <ankitshaw6933@gmail.com>
2025-06-21 14:05:22 +0530

    Navbar fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Services from "./components/services";
 import Testimonial from "./components/testimonial";
